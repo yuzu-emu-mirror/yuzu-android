51da014e53f84b465ea88cd9481504133f3ea166
yuzubot <yuzu@yuzu-emu.org>
2024-01-22 20:48:56 +0000

    Merge yuzu-emu#12499

--- src/common/wall_clock.h
+++ src/common/wall_clock.h
@@ -19,6 +19,8 @@ public:
 
     virtual ~WallClock() = default;
 
+    virtual void Reset() = 0;
+
     /// @returns The time in nanoseconds since the construction of this clock.
     virtual std::chrono::nanoseconds GetTimeNS() const = 0;
 
@@ -29,16 +31,13 @@ public:
     virtual std::chrono::milliseconds GetTimeMS() const = 0;
 
     /// @returns The guest CNTPCT ticks since the construction of this clock.
-    virtual u64 GetCNTPCT() const = 0;
+    virtual s64 GetCNTPCT() const = 0;
 
     /// @returns The guest GPU ticks since the construction of this clock.
-    virtual u64 GetGPUTick() const = 0;
+    virtual s64 GetGPUTick() const = 0;
 
     /// @returns The raw host timer ticks since an indeterminate epoch.
-    virtual u64 GetHostTicksNow() const = 0;
-
-    /// @returns The raw host timer ticks since the construction of this clock.
-    virtual u64 GetHostTicksElapsed() const = 0;
+    virtual s64 GetUptime() const = 0;
 
     /// @returns Whether the clock directly uses the host's hardware clock.
     virtual bool IsNative() const = 0;
